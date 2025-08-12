895e80c11b3287a1e93472fff649be694700569a
wangli <wangli858794774@gmail.com>
2025-08-12 11:28:24 +0800

    fix
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- .github/workflows/scripts/install_and_test.sh
+++ .github/workflows/scripts/install_and_test.sh
@@ -2,18 +2,28 @@
 
 # set -euo pipefail
 
-
+# check env
+echo "====> Check environment"
+echo "nic_name: $GLOO_SOCKET_IFNAME"
+echo "local_ip: $HCCL_IF_IP"
 # config mirror
 pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 export PIP_EXTRA_INDEX_URL=https://mirrors.huaweicloud.com/ascend/repos/pypi
 
-sleep 1000
+export WORKSPACE="/root/workspace"
 
-# cd $GITHUB_WORKSPACE/vllm_empty
-# VLLM_TARGET_DEVICE=empty pip install -e .
+cd $WORKSPACE
+# install dependencies
+apt-get -y install `cat packages.txt`
+apt-get -y install gcc g++ cmake libnuma-dev
 
-# cd $GITHUB_WORKSPACE
-# pip install -e .
+# install vllm
+cd $WORKSPACE/vllm-empty
+VLLM_TARGET_DEVICE=empty pip install -e .
 
-# # show
-# pip list | grep vllm
+# install vllm-ascend
+cd $WORKSPACE
+pip install -e .
+
+# show
+pip list | grep vllm
