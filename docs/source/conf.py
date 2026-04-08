facd79cb207b89d3fd204695869e942ca561aacc
wangli <wangli858794774@gmail.com>
2026-04-08 16:20:46 +0800

    upgrade vllm t0 0408
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,7 +80,7 @@ myst_substitutions = {
     "ci_vllm_version": "v0.18.0",
     # main branch compatibility matrix - updated dynamically
     # vLLM commit hash for main branch
-    "main_vllm_commit": "14acf429ac08b6d538ca6feb3e06b6d13895804d",
+    "main_vllm_commit": "5af684c31912232e5c89484c2e8259e0fac6c55b",
     # vLLM tag for main branch
     "main_vllm_tag": "",
     # Python version for main branch
